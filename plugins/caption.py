9725de041ba2965d10df4f649a5fdf84d68985b0
darshan00777 <120051379+darshan00777@users.noreply.github.com>
2022-12-10 01:28:02 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -4,19 +4,19 @@ from helper.database import db
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**__𝙶𝚒𝚟𝚎 𝚖𝚎 𝚊 𝚌𝚊𝚙𝚝𝚒𝚘𝚗 𝚝𝚘 𝚜𝚎𝚝.__\n\n𝙴𝚡𝚊𝚖𝚙𝚕𝚎:- `/set_caption {filename}\n\n💾 Size: {filesize}\n\n⏰ Duration: {duration}`**")
+       return await message.reply_text("**𝙶𝚒𝚟𝚎 𝚖𝚎 𝚊 𝚌𝚊𝚙𝚝𝚒𝚘𝚗 𝚝𝚘 𝚜𝚎𝚝.\n\n𝙴𝚡𝚊𝚖𝚙𝚕𝚎:- \n\n`/set_caption {filename}\n\n/set_caption {filesize}\n\n/set_caption {duration}`**")
     caption = message.text.split(" ", 1)[1]
     await db.set_caption(message.from_user.id, caption=caption)
-    await message.reply_text("__**✅ 𝚈𝙾𝚄𝚁 𝙲𝙰𝙿𝚃𝙸𝙾𝙽 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝚂𝙰𝚅𝙴𝙳**__")
+    await message.reply_text("**YOUR CAPTION SUCCESSFULLY SAVED ✅**")
 
     
 @Client.on_message(filters.private & filters.command('del_caption'))
 async def delete_caption(client, message):
     caption = await db.get_caption(message.from_user.id)  
     if not caption:
-       return await message.reply_text("😔**Sorry ! No Caption found...**😔")
+       return await message.reply_text("😔**Sorry! No Caption Found...**😔")
     await db.set_caption(message.from_user.id, caption=None)
-    await message.reply_text("**** Your Caption deleted successfully**✅️")
+    await message.reply_text("**YOUR CAPTION SUCCESSFULLY DELETED ✅**")
                                        
 @Client.on_message(filters.private & filters.command('see_caption'))
 async def see_caption(client, message):
@@ -24,4 +24,4 @@ async def see_caption(client, message):
     if caption:
        await message.reply_text(f"**Your Caption:-**\n\n`{caption}`")
     else:
-       await message.reply_text("😔**Sorry ! No Caption found...**😔")
+       await message.reply_text("😔**Sorry! No Caption Found...**😔")
